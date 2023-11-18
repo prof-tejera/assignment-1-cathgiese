a003f5969cd8b12e4609d9c07cb9ba3da797da14
Brandon Cloutier <bcloutier@protonmail.com>
2023-11-18 09:26:08 -0700

    fix lint errors

--- src/components/timers/Tabata.js
+++ src/components/timers/Tabata.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { useState, useEffect } from "react";
 import Button from "../generic/Button/Button";
 import DisplayTime from "../generic/DisplayTime/DisplayTime";
