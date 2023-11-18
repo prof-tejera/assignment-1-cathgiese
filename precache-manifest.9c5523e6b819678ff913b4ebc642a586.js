fabe62430a7965b3a6798f709de612f128c2ab6c
bcloutier <bcloutier@users.noreply.github.com>
2023-11-18 16:27:01 +0000

    deploy: a003f5969cd8b12e4609d9c07cb9ba3da797da14

---
+++ precache-manifest.9c5523e6b819678ff913b4ebc642a586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1aa35e6dc9d657142664929a86349d",
+    "url": "/assignment-1-cathgiese/index.html"
+  },
+  {
+    "revision": "3c9f949f5f4414866597",
+    "url": "/assignment-1-cathgiese/static/css/main.1f2c9f9f.chunk.css"
+  },
+  {
+    "revision": "a54ca287e89a50ab4c34",
+    "url": "/assignment-1-cathgiese/static/js/2.c58e1423.chunk.js"
+  },
+  {
+    "revision": "7a745f65e8ba20cc269c523bbf0fd5a3",
+    "url": "/assignment-1-cathgiese/static/js/2.c58e1423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9f949f5f4414866597",
+    "url": "/assignment-1-cathgiese/static/js/main.0c355d1d.chunk.js"
+  },
+  {
+    "revision": "c9be55e9ec9c329cc26b",
+    "url": "/assignment-1-cathgiese/static/js/runtime-main.1e881ca1.js"
+  }
+]);
